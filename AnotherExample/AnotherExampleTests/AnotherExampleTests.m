08ca3e84f31a49787e050c71695111d0ff30c83f
David Burns <david.burns@pega.com>
2014-02-17 18:21:02 -0500

    Yeah, some more for you…

---
+++ AnotherExample/AnotherExampleTests/AnotherExampleTests.m
@@ -0,0 +1,34 @@
+//
+//  AnotherExampleTests.m
+//  AnotherExampleTests
+//
+//  Created by David Burns on 2/17/14.
+//  Copyright (c) 2014 David Burns. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface AnotherExampleTests : XCTestCase
+
+@end
+
+@implementation AnotherExampleTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
