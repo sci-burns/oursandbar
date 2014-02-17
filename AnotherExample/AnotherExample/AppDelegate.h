a9d3fe4c452d511b9b3d0bcb4138b4af4976a2f6
David Burns <david.burns@pega.com>
2014-02-17 18:20:31 -0500

    Second commit.

---
+++ AnotherExample/AnotherExample/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  AnotherExample
+//
+//  Created by David Burns on 2/17/14.
+//  Copyright (c) 2014 David Burns. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
